5801cfd55cec601d75f580d1d78d27ec93925e9b
Abhishek <abhi@gmail.com>
2025-05-23 10:05:37 +0530

    Added Base url for frontend

--- src/main/java/com/olio/config/SecurityConfig.java
+++ src/main/java/com/olio/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://olio-client.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://olio-client.onrender.com",
+                "https://olio-frontend-gd85.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
